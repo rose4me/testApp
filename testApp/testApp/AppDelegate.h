73a55448be6b023250847d2b07dd3e4f22b6f422
rose4me <bala.rkh@gmail.com>
2017-01-17 09:53:57 -0600

    Add files via upload
    Initial draft

---
+++ testApp/testApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testApp
+//
+//  Created by Balasubramani Rajan on 1/17/17.
+//  Copyright © 2017 Balasubramani Rajan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ testApp/testApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testApp
+//
+//  Created by Balasubramani Rajan on 1/17/17.
+//  Copyright © 2017 Balasubramani Rajan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
